bddf0ba0bf90172924b118968f99280723266e0f
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-03-28 11:10:21 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    
+
+    
+    const btn = document.getElementById('button')
+    btn.addEventListener('click', () => {
+        btn.textContent = "clicked"
+        alert('Button was clicked')
+    })
+    
+    
+    
 }
+
